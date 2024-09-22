e4321d38c8e684d3d1360cab7a07b292394cb1f1
samtsee <samttsee@gmail.com>
2024-09-21 22:26:52 -0700

    set up basic web server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/samtsee/video-service/internal/api"
+)
+
+func main() {
+	app := api.New()
+
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app: %w", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/samtsee/video-service
 
 go 1.23.1
+
+require github.com/go-chi/chi/v5 v5.1.0
